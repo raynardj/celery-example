e42a4d200a469dd4af87acc8aa621b8fdb7f4a0b
raynardj <raynard@rasenn.com>
2020-01-02 16:40:42 +0800

    doc string

--- tasks.py
+++ tasks.py
@@ -6,9 +6,16 @@ app = Celery('tasks',backend = 'redis://localhost', broker='redis://localhost')
 
 @app.task
 def add(x, y):
+    """
+    simple calculation with intended occupied time
+    """
     sleep(10)
     return x + y
 
 @app.task
 def print_pid(input_id):
+    """
+    input_id: id, to distinguish input
+    return {id: process_id}
+    """
     return {input_id:os.getpid()}
